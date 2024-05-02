17f02dbee59471c0963dfee3bb1bae645093defe
wahabtlais <hussientlais20@gmail.com>
2024-05-02 16:29:45 +0300

    This commit adds several features to the getAllProducts controller in the product.controller.js file. These features include:
    "Add product filtering, sorting, limiting fields, and pagination to getAllProducts controller"

--- api/controller/product.controller.js
+++ api/controller/product.controller.js
@@ -27,7 +27,42 @@ export const getProduct = asyncHandler(async (req, res, next) => {
 
 export const getAllProducts = asyncHandler(async (req, res, next) => {
 	try {
-		const products = await Product.find({});
+		//* Filtering
+		const queryObj = { ...req.query };
+		const excludedFields = ["page", "sort", "limit", "fields"];
+		excludedFields.forEach((el) => delete queryObj[el]);
+		let queryStr = JSON.stringify(queryObj);
+		queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+		let query = Product.find(JSON.parse(queryStr));
+
+		//* Sorting
+		if (req.query.sort) {
+			const sortBy = req.query.sort.split(",").join(" ");
+			query = query.sort(sortBy);
+		} else {
+			query = query.sort("-createdAt");
+		}
+
+		//* Limiting the fields
+		if (req.query.fields) {
+			const fields = req.query.fields.split(",").join(" ");
+			query = query.select(fields);
+		} else {
+			query = query.select("-__v");
+		}
+
+		//* Pagination
+		const page = req.query.page;
+		const limit = req.query.limit;
+		const skip = (page - 1) * limit;
+		query = query.skip(skip).limit(limit);
+		if (req.query.page) {
+			const productCount = await Product.countDocuments();
+			if (skip >= productCount) throw new Error("This page does not exist!");
+		}
+
+		const products = await query;
 		res.json(products);
 	} catch (error) {
 		next(error);
